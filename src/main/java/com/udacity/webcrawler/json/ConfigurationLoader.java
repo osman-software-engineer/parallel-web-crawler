e898517e690258f9a712579e60b8c1efeacd15ec
OSMAN MOHAMMED <osman.softwareengineerintest@gmail.com>
2022-04-13 14:20:52 -0400

    chore:Added Udacity's Starter Code

---
+++ src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
@@ -0,0 +1,44 @@
+package com.udacity.webcrawler.json;
+
+import java.io.Reader;
+import java.nio.file.Path;
+import java.util.Objects;
+
+/**
+ * A static utility class that loads a JSON configuration file.
+ */
+public final class ConfigurationLoader {
+
+  private final Path path;
+
+  /**
+   * Create a {@link ConfigurationLoader} that loads configuration from the given {@link Path}.
+   */
+  public ConfigurationLoader(Path path) {
+    this.path = Objects.requireNonNull(path);
+  }
+
+  /**
+   * Loads configuration from this {@link ConfigurationLoader}'s path
+   *
+   * @return the loaded {@link CrawlerConfiguration}.
+   */
+  public CrawlerConfiguration load() {
+    // TODO: Fill in this method.
+
+    return new CrawlerConfiguration.Builder().build();
+  }
+
+  /**
+   * Loads crawler configuration from the given reader.
+   *
+   * @param reader a Reader pointing to a JSON string that contains crawler configuration.
+   * @return a crawler configuration
+   */
+  public static CrawlerConfiguration read(Reader reader) {
+    Objects.requireNonNull(reader);
+
+
+    return new CrawlerConfiguration.Builder().build();
+  }
+}
